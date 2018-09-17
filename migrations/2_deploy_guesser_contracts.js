32b0afdef3bc9c0c4ec40a4478194519479e9031
carlosgj94 <carlosgj94@gmail.com>
2018-09-17 12:24:31 +0200

    Adding the RegistrySetter in the migration files

--- migrations/2_deploy_guesser_contracts.js
+++ migrations/2_deploy_guesser_contracts.js
@@ -3,6 +3,7 @@ const BetPayments = artifacts.require("BetPayments");
 const BetOracle = artifacts.require("BetOracle");
 const BetTerms = artifacts.require("BetTerms");
 const ProxyRegistry = artifacts.require("ProxyRegistry");
+const RegistrySetter = artifacts.require("RegistrySetter");
 
 module.exports = function(deployer) {
     deployer.deploy(BetKernel);
@@ -10,4 +11,5 @@ module.exports = function(deployer) {
     deployer.deploy(BetOracle);
     deployer.deploy(BetTerms);
     deployer.deploy(ProxyRegistry);
+    deployer.deploy(RegistrySetter);
 };
--- migrations/3_contract_interaction.js
+++ migrations/3_contract_interaction.js
@@ -5,6 +5,7 @@ module.exports = function(deployer) {
     const BetOracle = artifacts.require("BetOracle");
     const BetTerms = artifacts.require("BetTerms");
     const ProxyRegistry = artifacts.require("ProxyRegistry");
+    const RegistrySetter = artifacts.require("RegistrySetter");
 
     // New Deployments
     const BetRegistry = artifacts.require("BetRegistry");
@@ -15,6 +16,7 @@ module.exports = function(deployer) {
         const oracle = await BetOracle.deployed();
         const terms = await BetTerms.deployed();
         const proxyRegistry = await ProxyRegistry.deployed();
+        const registrySetter = await RegistrySetter.deployed();
 
         // Deploying the registry
         await deployer.deploy(
@@ -32,5 +34,6 @@ module.exports = function(deployer) {
         await payments.setBetRegistry(registry.address);
         await oracle.setBetRegistry(registry.address);
         await terms.setBetRegistry(registry.address);
+        await registrySetter.setBetRegistry(registry.address);
     });
 };
